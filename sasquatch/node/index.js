2e8a2779f10c674380b5c523a333c080d8d8bb0d
Stuart Greenlief <mr.stubob@gmail.com>
2018-10-24 17:23:02 -0600

    broke everything out to SQL manager.

--- sasquatch/node/index.js
+++ sasquatch/node/index.js
@@ -1,7 +1,7 @@
 //Load express module with `require` directive
 let express = require('express');
 let app = express();
-let mysql = require('mysql');
+let sql = require('./sqlManager');
 let bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({extended: false}));
@@ -15,35 +15,14 @@ let _log = true;
 
 let dbOpts = {};
 
-function getQueryPromise(query, connection) {
-    return new Promise((res, rej) => {
-        connection.query(query, function (err, result) {
-            if (err) {rej(err);}
-            else {
-                res(result);
-            }
-        });
-    });
-}
-
 
 app.get('/manage', async (req, res) => {
-    let params = req.query;
-    let id = params.id;
+    let id = req.query.id;
     if (!id) {
         res.send({error: "required param: ID"});
     }
     else {
-        let sql = `SELECT *
-               FROM ${dbOpts.db}.${dbOpts.sightingsTbl} WHERE id =${id}`;
-        if (_log) {
-            console.log('params:', params);
-            console.log('received get request');
-            console.log('id:', id);
-            console.log('sql:', sql);
-        }
-        let response = await getQueryPromise(sql, connection);
-        console.log('response:', response);
+        let response = await sql.getSelectQueryPromise(id);
         res.send(response);
     }
 });
@@ -55,73 +34,44 @@ app.post('/manage', async (req, res) => {
         console.log('params:', params);
         console.log('received PUT request');
     }
-    //`test`.`Sightings` (`desc`, `lat`, `long`, `time`, `tags`) VALUES ('testing', 40, 111, 'NULL', 'NULL')
-
-    //TODO: validation
-    let sql = `INSERT INTO \`${dbOpts.db}\`.\`${dbOpts.sightingsTbl}\` (\`desc\`, \`lat\`, \`long\`, \`time\`, \`tags\`)
-               VALUES (
-               "${params.desc || "NULL"}"
-               ,"${params.lat || "NULL"}"
-               ,"${params.long || "NULL"}"
-               ,"${params.time || "NULL"}"
-               ,"${params.tags || "NULL"}")`;
-    if (_log) {console.log('sql:', sql);}
-    let response = await getQueryPromise(sql, connection);
+    //TODO: params validation
+    let response = await sql.getInsertQueryPromise(params, connection);
     if (_log) {console.log('response:', response);}
     res.send(response);
 });
 
 app.put('/manage', async (req, res) => {
     let params = req.body;
-
-    if (_log) {
-        console.log('params:', params);
-        console.log('received POST request');
-    }
-    //`test`.`Sightings` (`desc`, `lat`, `long`, `time`, `tags`) VALUES ('testing', 40, 111, 'NULL', 'NULL')
-
     let id = params.id;
     if (!id) {
         res.send({error: "required param: ID"});
     }
     else {
-        let setValues = [];
+        let response = await sql.getUpdateQueryPromise(id, params);
+        res.send(response);
+    }
+});
 
-        /*TODO: validation here*/
-        params.desc ? setValues.push(`t.\`desc\` = '${params.desc}'`) : "";
-        params.lat ? setValues.push(`t.\`lat\`   = ${params.lat}`) : "";
-        params.long ? setValues.push(`t.\`long\` = ${params.long}`) : "";
-        params.time ? setValues.push(`t.\`time\` = DATE('${params.time}')`) : "";
-        params.tags ? setValues.push(`t.\`tags\` = '${params.tags}'`) : "";
-
-        let sets = setValues.join(',');
-
-        let sql =
-            `UPDATE \`${dbOpts.db}\`.\`${dbOpts.sightingsTbl}\` t 
-            SET ${sets}
-            WHERE t.\`id\` = ${id}`;
-        console.log('sql:', sql);
-        let response = await getQueryPromise(sql, connection);
-        console.log('response:', response);
+app.delete('/manage', async (req, res) => {
+    let params = req.query;
+    let id = params.id;
+    if (!id) {
+        res.send({error: "required param: ID"});
+    }
+    else {
+        let response = await sql.getDeleteQueryPromise(id);
         res.send(response);
     }
 });
 
 
-app.delete('/manage', async (req, res) => {
-    let sql = `INSERT INTO test (name, address)
-               VALUES ('Company Inc', 'Highway 37')`;
-    connection.query(sql, function (err, result) {
-        if (err) throw err;
-        console.log("1 record inserted");
-    });
-});
-
-
-app.get('/distance', (req, res) => {
+app.get('/related', async (req, res) => {
     //change 3959 with 6371 to do kilometers
     //from https://stackoverflow.com/questions/4687312/querying-within-longitude-and-latitude-in-mysql
-    let lat = 1;
+
+    let params = req.query;
+    let target = await sql.getSelectQueryPromise(params.id);
+    let lat = params;
     let lng = 1;
     let distance = 20;
     let limit = 25;
@@ -133,25 +83,7 @@ app.get('/distance', (req, res) => {
 
 
 app.listen(port, function () {
-    dbOpts.con = {
-        host: process.env.MYSQL_HOST,
-        user: process.env.MYSQL_USER,
-        password: process.env.MYSQL_PASSWORD,
-        database: process.env.MYSQL_DATABASE,
-    };
-    dbOpts.db = process.env.MYSQL_DATABASE;
-    dbOpts.sightingsTbl = "Sightings";
-    connection = mysql.createConnection(dbOpts.con);
-    connection.connect(function (err) {
-        if (err) {
-            console.log('unable to connect to MySQL - this is probobly because is isn\'t up yet');
-            throw error('unable to connect to MySQL');
-        } else {
-            console.log('Connected to MySQL - Thread ' + connection.threadId);
-        }
-    });
-
-
+    sql.startSQLConnection();
     console.log(`app listening on port ${port}!`);
 });
 
@@ -160,7 +92,7 @@ app.listen(port, function () {
 
 function exitHandler(options, exitCode) {
     console.log('exiting');
-    //connection.end();
+    //
 }
 
 //do something when app is closing
---
+++ sasquatch/node/sqlManager.js
@@ -0,0 +1,98 @@
+let mysql = require('mysql');
+
+
+
+let dbOpts = {};
+
+function startSQLConnection(options){
+    options = options || {};
+    dbOpts.connectionSettings = Object.assign({
+        host: process.env.MYSQL_HOST,
+        user: process.env.MYSQL_USER,
+        password: process.env.MYSQL_PASSWORD,
+        database: process.env.MYSQL_DATABASE,
+    }, options);
+    dbOpts.db = dbOpts.connectionSettings.db;
+    dbOpts.tbl = options.tbl || "Sightings";
+    dbOpts.connection = mysql.createConnection(dbOpts.connectionSettings);
+    dbOpts.connection.connect(function (err) {
+        if (err) {
+            console.log('unable to connect to MySQL - this is probably because is isn\'t up yet');
+            throw error('unable to connect to MySQL');
+        } else {
+            console.log('Connected to MySQL - Thread ' + dbOpts.connection.threadId);
+        }
+    });
+}
+
+function closeSQLConnection(){
+    dbOpts.connection.end();
+}
+
+function getQueryPromise(query, connection) {
+    return new Promise((res, rej) => {
+        connection.query(query, function (err, result) {
+            if (err) {rej(err);}
+            else {
+                res(result);
+            }
+        });
+    });
+}
+
+function getSelectQueryPromise(id, options){
+    let opts = Object.assign({}, options, dbOpts);
+    return getQueryPromise(
+    `SELECT *
+    FROM ${opts.db}.${opts.tbl}
+    WHERE id =${id}`, opts.connection)
+}
+function getInsertQueryPromise(values, options){
+    let opts = options || dbOpts;
+    let con = connection || connection;
+    let query = `INSERT INTO \`${dbOpts.db}\`.\`${dbOpts.tbl}\` (\`desc\`, \`lat\`, \`long\`, \`time\`, \`tags\`)
+               VALUES (
+               "${values.desc || "NULL"}"
+               ,"${values.lat || "NULL"}"
+               ,"${values.long || "NULL"}"
+               ,"${values.time || "NULL"}"
+               ,"${values.tags || "NULL"}")`;
+    return getQueryPromise(query, con);
+}
+
+function getUpdateQueryPromise(id, values, options){
+    let opts = Object.assign({}, options, dbOpts);
+    let setValues = [];
+    values.desc ? setValues.push(`t.\`desc\` = '${params.desc}'`) : "";
+    values.lat ? setValues.push(`t.\`lat\`   = ${params.lat}`) : "";
+    values.long ? setValues.push(`t.\`long\` = ${params.long}`) : "";
+    values.time ? setValues.push(`t.\`time\` = DATE('${params.time}')`) : "";
+    values.tags ? setValues.push(`t.\`tags\` = '${params.tags}'`) : "";
+    let sets = setValues.join(',');
+
+    let sql =
+        `UPDATE \`${dbOpts.db}\`.\`${dbOpts.tbl}\` t 
+            SET ${sets}
+            WHERE t.\`id\` = ${id}`;
+    return getQueryPromise(sql, opts.connection);
+}
+
+function getDeleteQueryPromise(id, options){
+    let opts = Object.assign({}, options, dbOpts);
+    let sql =
+        `DELETE FROM ${dbOpts.db}.${dbOpts.tbl} WHERE id=${id}`;
+    return getQueryPromise(sql, opts.connection);
+}
+
+module.exports= {
+    dbOpts : dbOpts,
+    connection: dbOpts.connection,
+    getQueryPromise : getQueryPromise,
+    getSelectQueryPromise : getSelectQueryPromise,
+    getInsertQueryPromise : getInsertQueryPromise,
+    getUpdateQueryPromise : getUpdateQueryPromise,
+    getDeleteQueryPromise : getDeleteQueryPromise,
+    startSQLConnection : startSQLConnection,
+
+
+};
